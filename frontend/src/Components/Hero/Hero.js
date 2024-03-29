621c91c6b9717b0a87d1bde0cbe56f61a65c3937
vbhagat123 <vrajbhagat42@gmail.com>
2024-03-28 22:12:08 -0700

    updated the link for hero button

--- frontend/src/Components/Hero/Hero.js
+++ frontend/src/Components/Hero/Hero.js
@@ -1,9 +1,13 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import './Hero.css'
 import arrow_icon from '../Assets/right-arrow.png'
 import hero_image from '../Assets/hero.png'
 
 function Hero() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
   return (   
     <div className='hero'>
         <div className='hero-left'>
@@ -14,7 +18,9 @@ function Hero() {
             </div>
             <div>
                 <button className="hero-latest-button">
-                  <a href="/Senior_Project_2024/NewCollection">Latest Collection</a>
+                  <NavLink to="/NewCollection" className="login-message" onClick={handleClick}>
+                  Latest Collection
+                  </NavLink>
                   <img src={arrow_icon} alt="" />
                   </button>
             </div>
